2e619d599baa92246a1c9f930c1e1254a0985212
Manav Garg <58691392+ManavvGarg@users.noreply.github.com>
2023-09-27 00:23:03 +0530

    Day-3

--- Day-1/ListNode.java
+++ Day-1/ListNode.java
@@ -1,12 +1,21 @@
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
 class Solution {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode prehead = new ListNode(-1);
         ListNode cur = prehead;
 
         while(l1 != null && l2 != null) {
             if(l1.val <= l2.val) {
                 cur.next = l1;
-                l1 = l1.next
+                l1 = l1.next;
             }
 
             else {
@@ -14,7 +23,7 @@ class Solution {
                 l2 = l2.next;
             }
 
-            cur = cur.next
+            cur = cur.next;
         }
 
         cur.next = l1 == null ? l2 : l1;
